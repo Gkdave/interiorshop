8fc9a541c5de23e91e98c0f17cab20a1f11ca81c
gajendra <davegk59@gmil.com>
2023-09-06 10:30:00 +0530

    first commit

---
+++ shop/core/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+# from django.apps import CoreConfig
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shop.core'
