9a2a9d221adbf81cb775bbc8faf37e71487572ac
gajendra <davegk59@gmil.com>
2023-09-08 15:37:40 +0530

    show list of products in admin area

--- shop/vendor/views.py
+++ shop/vendor/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect 
 
 from .models import Vendor
+from shop.product.models import Product 
 
 
 def become_vendor(request):
@@ -25,8 +26,9 @@ def become_vendor(request):
 @login_required
 def vendor_admin(request):
     vendor = request.user.vendor 
+    products = vendor.products.all()
     
-    return render(request,'vendor/vendor_admin.html',{'vendor':vendor})
+    return render(request,'vendor/vendor_admin.html',{'vendor':vendor, 'products': products})
     
         
         
