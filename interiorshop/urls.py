f9cb0c727fafc07855ed88f977411a1cce918102
gajendra <davegk59@gmil.com>
2023-09-08 17:56:23 +0530

    newest products

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -13,7 +13,13 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    
 """
+# gajendra    dave@123
+# bhawana     admin@123
+# himanshu    gajendra@12
+# ravindra    admin@12
+
 from django.conf import settings 
 from django.contrib import admin
 from django.urls import path,include 
