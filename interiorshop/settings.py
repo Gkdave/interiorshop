b3fcffdc6357cd033ee812a6df7794e96a4ffdd0
gajendra <davegk59@gmil.com>
2023-09-11 16:56:35 +0530

    added cart

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shop.product.context_processors.menu_categories',
+                'shop.cart.context_processors.cart',
             ],
         },
     },
